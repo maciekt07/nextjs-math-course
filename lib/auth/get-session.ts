f6ceafe354e48a8cfe86d7f00c0738413aa1eae1
maciekt07 <85953204+maciekt07@users.noreply.github.com>
2026-03-05 21:31:39 +0100

    feat(auth): add params to getServerSession

--- lib/auth/get-session.ts
+++ lib/auth/get-session.ts
@@ -2,6 +2,13 @@ import { headers } from "next/headers";
 import { cache } from "react";
 import { auth } from "./auth";
 
-export const getServerSession = cache(async () => {
-  return await auth.api.getSession({ headers: await headers() });
-});
+type GetSessionParams = Parameters<typeof auth.api.getSession>[0];
+
+export const getServerSession = cache(
+  async (params?: Omit<GetSessionParams, "headers">) => {
+    return auth.api.getSession({
+      ...params,
+      headers: await headers(),
+    });
+  },
+);
