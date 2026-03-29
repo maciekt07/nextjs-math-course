727b804318992d8176c0e3775233032c73bb2431
maciekt07 <85953204+maciekt07@users.noreply.github.com>
2026-03-29 19:25:22 +0200

    perf(cms): skip revalidation for drafts

--- cms/hooks/revalidateCourse.ts
+++ cms/hooks/revalidateCourse.ts
@@ -1,13 +1,19 @@
 import { revalidateTag } from "next/cache";
 import type { CollectionAfterChangeHook } from "payload";
+import type { Course } from "@/types/payload-types";
 
-export const revalidateCourse: CollectionAfterChangeHook = async ({
+export const revalidateCourse: CollectionAfterChangeHook<Course> = async ({
   doc,
+  previousDoc,
   req: { payload, context },
 }) => {
   if (context.disableRevalidate) return doc;
 
   const tag = "courses-list";
+
+  if (doc._status === "draft" && previousDoc._status === "draft") {
+    return doc;
+  }
 
   payload.logger.info(`Revalidating tag: ${tag} for course: ${doc.slug}`);
   revalidateTag(tag, "max");
--- cms/hooks/revalidateLesson.ts
+++ cms/hooks/revalidateLesson.ts
@@ -1,8 +1,10 @@
 import { revalidateTag } from "next/cache";
 import type { CollectionAfterChangeHook } from "payload";
+import type { Lesson } from "@/types/payload-types";
 
-export const revalidateLesson: CollectionAfterChangeHook = async ({
+export const revalidateLesson: CollectionAfterChangeHook<Lesson> = async ({
   doc,
+  previousDoc,
   req: { payload, context },
 }) => {
   if (context.disableRevalidate) return doc;
@@ -13,6 +15,10 @@ export const revalidateLesson: CollectionAfterChangeHook = async ({
   }
 
   const tag = `lesson:${doc.id}`;
+
+  if (doc._status === "draft" && previousDoc._status === "draft") {
+    return doc;
+  }
 
   payload.logger.info(`Revalidating tag: ${tag} for lesson ${doc.slug}`);
   revalidateTag(tag, "max");
