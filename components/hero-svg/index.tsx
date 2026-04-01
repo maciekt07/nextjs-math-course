11adb1e8e633bb7b610fede061e5d13ed437fdf6
maciekt07 <85953204+maciekt07@users.noreply.github.com>
2026-04-01 16:00:27 +0200

    perf(hero-svg): requestIdleCallback

--- components/hero-svg/index.tsx
+++ components/hero-svg/index.tsx
@@ -1,25 +1,40 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import type { SVGProps } from "react";
+import { type SVGProps, useEffect, useState } from "react";
 import Floor from "@/components/hero-svg/floor";
 
 const HeroElements = dynamic(() => import("./elements"), { ssr: false });
 
-const HeroSvgComponent = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    width={500}
-    height={500}
-    viewBox="0 0 500 500"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <g id="Hero-SVG">
-      {/* floor is SSR */}
-      <Floor />
-      <HeroElements />
-    </g>
-  </svg>
-);
+const HeroSvgComponent = (props: SVGProps<SVGSVGElement>) => {
+  const [show, setShow] = useState<boolean>(false);
+  useEffect(() => {
+    const cb = () => setShow(true);
+
+    if ("requestIdleCallback" in window) {
+      const id = requestIdleCallback(cb);
+      return () => cancelIdleCallback(id);
+    } else {
+      const id = setTimeout(cb, 500);
+      return () => clearTimeout(id);
+    }
+  }, []);
+
+  return (
+    <svg
+      width={500}
+      height={500}
+      viewBox="0 0 500 500"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="Hero-SVG">
+        {/* floor is SSR */}
+        <Floor />
+        {show && <HeroElements />}
+      </g>
+    </svg>
+  );
+};
 export default HeroSvgComponent;
